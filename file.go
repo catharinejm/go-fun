1bfbcec2d167195da5260d716f4e6b6b05929a7a
Jon Distad <jon.distad@gmail.com>
2011-01-14 14:48:49 -0500

    File example

--- file.go
+++ file.go
@@ -31,3 +31,41 @@ func Open(name string, mode int, perm uint32) (file *File, err os.Error) {
   }
   return newFile(r, name), err
 }
+
+func (file *File) Close() os.Error {
+  if file == nil {
+    return os.EINVAL
+  }
+  e := syscall.Close(file.fd)
+  file.fd = -1
+  if e != 0 {
+    return os.Errno(e)
+  }
+  return nil
+}
+
+func (file *File) Read(b []byte) (ret int, err os.Error) {
+  if file == nil {
+    return -1, os.EINVAL
+  }
+  r, e := syscall.Read(file.fd, b)
+  if e != 0 {
+    err = os.Errno(e)
+  }
+  return int(r), err
+}
+
+func (file *File) Write(b []byte) (ret int, err os.Error) {
+  if file == nil {
+    return -1, os.EINVAL
+  }
+  r, e := syscall.Write(file.fd, b)
+  if e != 0 {
+    err = os.Errno(e)
+  }
+  return int(r), err
+}
+
+func (file *File) String() string {
+  return file.name
+}
